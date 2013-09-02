3d4ef23ffcd30705602ff2f5d74c0f646dcf8da1
Alan <linpd2@gmail.com>
2013-09-02 10:19:05 -0400

    Added class

--- union-find/client.js
+++ union-find/client.js
@@ -1,17 +1,39 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function(chunk) {
-  var elements = chunk.split('\n');
-  if (elements.length < 1) {
-    return;
-  }
 
-  var numObjects = elements[0];
-  console.log('Number of objects: ' + numObjects);
-  for (var i = 1; i < elements.length; i++) {
-    if (elements[i]) {
-      console.log(elements[i].split(' '));
+function UF() {
+  this.sites = [];
+}
+
+UF.prototype.find = function(a, b) {
+  return null;
+}
+
+UF.prototype.union = function(a, b) {
+  return null;
+}
+
+UF.prototype.numComponents = function() {
+  return null;
+}
+
+UF.prototype.init = function() {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', function(chunk) {
+    var elements = chunk.split('\n');
+    if (elements.length < 1) {
+      return;
     }
-  }
 
-});
+    var numObjects = elements[0];
+    console.log('Number of objects: ' + numObjects);
+    for (var i = 1; i < elements.length; i++) {
+      if (elements[i]) {
+        console.log(elements[i].split(' '));
+      }
+    }
+
+  });
+}
+
+var uf = new UF();
+uf.init();
