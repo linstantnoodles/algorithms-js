bdd4832ae83fd20565330b121770844f10d5ac12
Alan <linpd2@gmail.com>
2013-09-02 10:36:48 -0400

    Added basic implementations

--- union-find/client.js
+++ union-find/client.js
@@ -4,18 +4,33 @@ function UF() {
 }
 
 UF.prototype.find = function(a, b) {
-  return null;
+  // TODO: Add checks
+  return this.sites[a] == this.sites[b];
 }
 
 UF.prototype.union = function(a, b) {
-  return null;
+  // value of b
+  var vB = this.sites[b];
+  var vA = this.sites[a];
+  for (var i = 0; i < this.sites.length; i++) {
+    if (this.sites[i] == vA) {
+      this.sites[i] = vB;
+    }
+  }
 }
 
 UF.prototype.numComponents = function() {
-  return null;
+  return 5;
+}
+
+UF.prototype.createSites = function(n) {
+  for (var i = 0; i < n; i++) {
+    this.sites[i] = i;
+  }
 }
 
 UF.prototype.init = function() {
+  var _this = this;
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', function(chunk) {
@@ -24,11 +39,14 @@ UF.prototype.init = function() {
       return;
     }
 
-    var numObjects = elements[0];
-    console.log('Number of objects: ' + numObjects);
+    _this.createSites(elements[0]);
     for (var i = 1; i < elements.length; i++) {
       if (elements[i]) {
-        console.log(elements[i].split(' '));
+        var pair = elements[i].split(' ');
+        if (!_this.find(pair[0], pair[1])) {
+          console.log("Connecting sites: " + pair[0] + ", " + pair[1]);
+          _this.union(pair[0], pair[1]);
+        }
       }
     }
 
