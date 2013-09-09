74d640f81b631675702a589a78a959b889bfd90f
Alan <linpd2@gmail.com>
2013-09-09 12:13:51 -0400

    Added stats client

---
+++ union-find/percolation-stats.js
@@ -0,0 +1,31 @@
+// The statistical engine
+// @param {number} n width of system
+// @param {number} t number of runs
+function PercolationStats(n, t) {
+
+}
+
+PercolationStats.prototype.mean = function() {
+
+}
+
+PercolationStats.prototype.stddev = function() {
+
+}
+
+PercolationStats.prototype.confidenceLo = function() {
+
+}
+
+PercolationStats.prototype.confidenceHi = function() {
+
+}
+
+PercolationStats.prototype.init = function() {
+  // Execute the client and get the data
+  // Create client
+  // for ( number of runs )
+    // execute client. Return threshhold value.
+
+  // Compute results
+}
