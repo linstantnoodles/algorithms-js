8ed6581d2e49e73d260599f6c37824cdf9008263
Alan <linpd2@gmail.com>
2013-09-11 10:36:05 -0400

    Removed unused arg parsing and minor formatting fixes

--- binary-search/client.js
+++ binary-search/client.js
@@ -1,7 +1,3 @@
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ': ' + val);
-});
-
 function getRandomSortedArray() {
   var list = new Array(10);
   // Fill with rand numbers
@@ -18,21 +14,23 @@ function getRandomSortedArray() {
 
 var list = getRandomSortedArray();
 var randTarget = list[Math.floor(Math.random() * list.length)];
+
 console.log(list);
 console.log(randTarget);
+
 function binarySearch(list, target) {
   var l = 0;
   var r = list.length - 1;
   while (l <= r) {
     var mid = Math.floor((l + r) / 2);
     if (target < list[mid]) {
-      console.log("Go left");
+      console.log('Go left');
       r = mid - 1;
     } else if (target > list[mid]) {
-      console.log("Go right");
+      console.log('Go right');
       l = mid + 1;
     } else {
-      console.log("Found at : " + mid);
+      console.log('Found at : ' + mid);
       return true;
     }
   }
