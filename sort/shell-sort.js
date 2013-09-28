3090fcbd431af0de71be2a0220fa0c663405cbb0
Alan <linpd2@gmail.com>
2013-09-28 12:12:24 -0400

    added shellsort

---
+++ sort/shell-sort.js
@@ -0,0 +1,29 @@
+var list = [12,2,5,2,1,5,6,12,35,56,55,32,1,3,43,1];
+
+function getPrevSeq(val) {
+  return Math.floor((val - 1) / 3);
+}
+
+function shellSort(list) {
+  var listLen = list.length;
+  var seq = 3 * getPrevSeq(listLen - 1) + 1;
+  while (seq > 0) {
+    console.log("Seq: " + seq);
+    for (var i = seq; i < listLen; i+=seq) {
+      var j = i;
+      while (list[j] < list[j-seq]) {
+        var temp = list[j];
+        list[j] = list[j-seq];
+        list[j-seq] = temp;
+        j-=seq;
+      }
+    }
+    seq = getPrevSeq(seq);
+  }
+}
+
+console.log(list);
+shellSort(list);
+console.log(list);
+
+
