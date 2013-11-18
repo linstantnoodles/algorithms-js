f604b5036652675db88bb09a2b0dc6a7e7c736f5
Alan <linpd2@gmail.com>
2013-11-18 09:20:03 -0500

    cleanup

--- sort/merge-sort.js
+++ sort/merge-sort.js
@@ -1,26 +1,27 @@
 var list = [2,5,6,7,1,3,4];
 
-function merge(list, leftLo, leftHi, rightLo, rightHi) {
+function merge(list, lo, mid, hi) {
   var sortedList = [];
-  var left = leftLo;
+  var left = lo;
+  var rightLo = mid + 1;
 
-  while (leftLo <= leftHi && rightLo <= rightHi) {
+  while (lo <= mid && rightLo <= hi) {
     // Maintains stability by moving from left subarray for equal keys
-    if (list[leftLo] <= list[rightLo]) {
-      sortedList.push(list[leftLo++]);
+    if (list[lo] <= list[rightLo]) {
+      sortedList.push(list[lo++]);
     } else {
       sortedList.push(list[rightLo++]);
     }
   }
-
-  while (leftLo <= leftHi) {
-    sortedList.push(list[leftLo++]);
+  // Push remaining values
+  while (lo <= mid) {
+    sortedList.push(list[lo++]);
   }
 
-  while (rightLo <= rightHi) {
+  while (rightLo <= hi) {
     sortedList.push(list[rightLo++]);
   }
-
+  // Copy to original array
   while (sortedList.length > 0) {
     list[left++] = sortedList.shift();
   }
@@ -34,7 +35,7 @@ function mergeSort(list, lo, hi) {
   var mid = Math.floor((lo + hi) / 2);
   mergeSort(list, lo, mid);
   mergeSort(list, mid + 1, hi);
-  merge(list, lo, mid, mid + 1, hi);
+  merge(list, lo, mid, hi);
   return;
 }
 
--- sort/selection-sort.js
+++ sort/selection-sort.js
@@ -1,18 +1,21 @@
-var list = [5,4,2,5,1,8];
+var list = [5,4,2,5,1,8,8,8,5];
 
 function selectionSort(list) {
   var minIndex;
   var listLen = list.length;
-  for (var i = 0; i < listLen; i++) {
+  // Last element will be in correct pos
+  for (var i = 0; i < listLen - 1; i++) {
     minIndex = i;
     for (var j = i + 1; j < listLen; j++) {
       if (list[j] < list[i]) {
         minIndex = j;
       }
     }
-    var temp = list[i];
-    list[i] = list[minIndex];
-    list[minIndex] = temp;
+    if (minIndex != i) {
+      var temp = list[i];
+      list[i] = list[minIndex];
+      list[minIndex] = temp;
+    }
   }
 }
 
