662c0dd10b9a1c3a353068a3a47e1c5b7d5befdd
Alan <linpd2@gmail.com>
2013-11-18 13:37:19 -0500

    find prime factors

---
+++ misc/prime_factors.js
@@ -0,0 +1,22 @@
+function primeFactors(n) {
+  if (n == 1) return [];
+
+  var divisor = 2;
+  var primeFactors = [];
+
+  while (n > 1) {
+      if (n % divisor == 0) {
+          n = n / divisor;
+          primeFactors.push(divisor);
+      } else {
+          divisor++;
+      }
+  }
+
+  return primeFactors;
+}
+
+for(var i = 1; i < 20; i++) {
+  console.log(i + " : ");
+  console.log(primeFactors(i));
+}
