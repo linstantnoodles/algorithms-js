8ddab6839bb59f7050e49c19bab3b241ff2a013c
Alan <linpd2@gmail.com>
2014-02-08 11:15:59 -0500

    fixed comments and added sort flag

--- util/array.js
+++ util/array.js
@@ -1,16 +1,22 @@
-/*
+/**
  * Array utility functions
  */
 
 var Utility = {};
 
-/*
- * @return Array random numbers [0, range)
+/**
+ * Return an array of random numbers within interval [0, range]
+ *
+ * @param size {Number} size of array
+ * @param range {Number} range of random values
+ * @param sort {Boolean} whether to sort the array
+ * @return {Array}
  */
-Utility.randomNumbers = function(size, range) {
+
+Utility.randomNumbers = function(size, range, sort) {
   var size = size || 100;
   var range = range || 100;
-
+  var comparator = function(a, b) { return a - b; };
   var list = [];
 
   for (var i = 0; i < size; i++) {
@@ -18,7 +24,7 @@ Utility.randomNumbers = function(size, range) {
     list.push(newNum);
   }
 
-  return list;
+  return (sort) ? list.sort(comparator) : list;
 }
 
 module.exports = Utility;
